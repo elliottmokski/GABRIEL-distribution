ee763905db465ce7f3271ad10f8349a3cb2ffcee
Elliott Mokski <emokski@college.harvard.edu>
2024-03-22 08:14:10 -0400

    fix DS_Store

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'GABRIEL': ['Prompts/*.jinja'],
+        'GABRIEL': ['Prompts/*.j2'],
     },
     install_requires=[
         'numpy>=1.18.0',
