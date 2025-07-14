03e2f3f7fcc9a599945ba402baaa72108f6e3c5b
Elliott Mokski <112282343+elliottmokski@users.noreply.github.com>
2025-07-14 15:10:28 -0700

    Add ratings and deidentification tasks

--- src/gabriel/__init__.py
+++ src/gabriel/__init__.py
@@ -1,12 +1,15 @@
 """GABRIEL: LLM-based social science analysis toolkit."""
 
 from .tasks.simple_rating import SimpleRating
+from .tasks.ratings import Ratings
+from .tasks.deidentification import Deidentifier
 from .tasks.elo import EloRater
-
 from .tasks.identification import Identification
 
 __all__ = [
     "SimpleRating",
     "EloRater",
+    "Ratings",
+    "Deidentifier",
     "Identification",
 ]
